fe5d2e369de3b26937739f421fd91351566a96a5
yumetodo <yume-wikijp@live.jp>
2024-09-22 05:34:03 +0900

    fix(ui): ボタン連打対策 (#5)

--- src/renderer/src/components/Home.tsx
+++ src/renderer/src/components/Home.tsx
@@ -5,6 +5,7 @@ function Home(): JSX.Element {
   const navigate = useNavigate()
 
   const [projectName, setProjectName] = useState<string>('')
+  const [recording, setRecording] = useState<boolean>(false)
   // WARN: 動画再生のテストのための処理であるため後で置き換える
   const [videoFilePath, setVideoFilePath] = useState<string | null>(null)
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
@@ -17,10 +18,14 @@ function Home(): JSX.Element {
   }
 
   const startRecord = (): void => {
+    if (recording) return
+    setRecording(true)
     window.api.startRecord(projectName)
   }
-  const stopRecord = (): void => {
-    window.api.stopRecord()
+  const stopRecord = async (): Promise<void> => {
+    if (!recording) return
+    await window.api.stopRecord()
+    setRecording(false)
   }
 
   return (
@@ -29,8 +34,8 @@ function Home(): JSX.Element {
       <section>
         <h2>キャプチャ</h2>
         <input type="text" onChange={(e) => setProjectName(e.target.value)} />
-        <button onClick={startRecord}>録画開始</button>
-        <button onClick={stopRecord}>録画終了</button>
+        <button disabled={recording} onClick={startRecord}>録画開始</button>
+        <button disabled={!recording} onClick={stopRecord}>録画終了</button>
       </section>
       <div>
         <h3>動画再生テスト用ファイルのアップロード</h3>
