3a259a51f2cecfa03a67039bb2dae73bd4cdadf1
YuichirouSeitoku <kazeso.kazewo@gmail.com>
2024-09-21 14:59:52 +0900

    2画面間を行き来できる処理を実装(#1)

---
+++ src/renderer/src/components/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/sample');
+  };
+
+  return (
+    <div>
+      <h1>ホーム画面</h1>
+      <button onClick={handleClick}>別の画面へ移動</button>
+    </div>
+  );
+}
+
+export default Home;
