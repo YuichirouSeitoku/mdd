43eebd9d49c8f4c0d09c7fab55425c7b3d57d5b5
yumetodo <yume-wikijp@live.jp>
2024-09-21 21:22:59 +0900

    feat: shortcut監視クラスの初期実装 (#4)

---
+++ src/main/shortcut.ts
@@ -0,0 +1,60 @@
+import {
+  GlobalKeyboardListener,
+  IGlobalKeyEvent,
+  IGlobalKeyDownMap
+} from 'node-global-key-listener'
+import microtime from 'microtime'
+
+export type OnShortcutHandler = (keys: string[], eventAt: number) => void
+
+export class ShortcutWatcher {
+  listener: GlobalKeyboardListener | null
+  onShortcutHandler: OnShortcutHandler
+  requiredKeys: Readonly<string[]>
+  previousEventAt: number | null
+  constructor(requiredKeys: Readonly<string[]>, onShortcutHandler: OnShortcutHandler) {
+    this.requiredKeys = requiredKeys
+    this.onShortcutHandler = onShortcutHandler
+    this.previousEventAt = null
+    this.listener = null
+  }
+
+  start(): void {
+    this.listener = new GlobalKeyboardListener()
+    this.listener.addListener(this.handler.bind(this))
+    this.previousEventAt = null
+  }
+
+  dispose(): void {
+    if (this.listener == null) return
+    this.listener.kill()
+    this.listener = null
+    this.previousEventAt = null
+  }
+
+  private handler(event: IGlobalKeyEvent, isDown: IGlobalKeyDownMap): void {
+    // まず時刻を求める
+    const now = microtime.now()
+    if (event.state !== 'UP') {
+      // DOWNの場合
+      this.previousEventAt = now
+      return
+    }
+    // 前回イベント時刻が不明な場合はskip
+    if (this.previousEventAt == null) return
+    if (event.name == null || !this.requiredKeys.some((k) => isDown[k] === true)) return
+
+    const currentKeys = Object.entries(isDown)
+      .filter(([_, v]) => v === true)
+      .map(([k, _]) => k)
+    currentKeys.push(event.name)
+
+    try {
+      this.onShortcutHandler(currentKeys, this.previousEventAt)
+    } finally {
+      // 以降の一連のUPイベントをSKIPさせる
+      this.previousEventAt = null
+    }
+    return
+  }
+}
