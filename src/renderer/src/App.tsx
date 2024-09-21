ed55b40f92e9e2bf9a4a8f459f6dd0de2c759128
YuichirouSeitoku <kazeso.kazewo@gmail.com>
2024-09-21 17:31:16 +0900

    宣言したのに使われていない値を削除(#13)

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Sample from './components/Sample'
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
     <Router>
       <Routes>
--- src/renderer/src/components/Sample.tsx
+++ src/renderer/src/components/Sample.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
-function Sample(): JSX.Element {
+function Sample() {
   return (
     <div>
       <h1>Sample画面</h1>
     </div>
-  )
+  );
 }
 
-export default Sample
+export default Sample;
