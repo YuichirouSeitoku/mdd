d7238ce506a070ff70e28dd0cff4bab760f119ca
gauzesya <gauzesya@gmail.com>
2024-09-21 21:19:59 +0900

    🔨 Dev環境実行時に開発者ツールが開くように変更 (#2)

--- src/main/index.ts
+++ src/main/index.ts
@@ -27,6 +27,11 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
+  // NOTE: dev環境では開発者ツールを開く
+  if (is.dev) {
+    mainWindow.webContents.openDevTools()
+  }
+
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
